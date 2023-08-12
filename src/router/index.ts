27b94e22f02d08b70fec3adcf0f8913832f1f38e
HaHa5233 <g2636408817@163.com>
2023-08-12 22:21:25 +0800

    修改home路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/home/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/home',
+      name: 'Home',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('@/views/AboutView.vue')
+    // }
   ]
 })
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import LeftMenu from '@/components/left-menu/index.vue'
+defineExpose({})
+
+onMounted(() => { })
+onUnmounted(() => { })
+</script>
+
+<template>
+  <div>
+    <el-row :gutter="0">
+      <el-col :span="6">
+        <left-menu />
+      </el-col>
+      <el-col :span="18"></el-col>
+    </el-row>
+  </div>
+</template>
